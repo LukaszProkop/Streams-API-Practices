3f8b905669dd826c83fd6c03471f4556f98e6ed0
Lukasz_Prokop <Lukasz_Prokop@epam.com>
2024-10-31 21:00:04 +0100

    adjust code

--- src/test/java/com/java/stream/interview/EmployeeWorkedForMaxProjectTest.java
+++ src/test/java/com/java/stream/interview/EmployeeWorkedForMaxProjectTest.java
@@ -3,7 +3,9 @@ package com.java.stream.interview;
 import com.java.stream.interview.domain.CorporateEmployee;
 import com.java.stream.interview.domain.Project;
 import com.java.stream.solutions.InterviewProblemSolutions;
+
 import java.util.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -20,25 +22,31 @@ import org.junit.jupiter.api.Test;
  * return any one of the tied employees.
  */
 public class EmployeeWorkedForMaxProjectTest {
-  @Test
-  @Disabled()
-  public void numberOfOccurencesOfEachCharacter() {
-    final var project1 =
-        List.of(
-            new Project("E1", 2), new Project("E2", 1), new Project("E3", 2), new Project("E4", 2));
+    @Test
+    @Disabled()
+    public void numberOfOccurencesOfEachCharacter() {
+        final var project1 =
+                List.of(
+                        new Project("E1", 2),
+                        new Project("E2", 1),
+                        new Project("E3", 2),
+                        new Project("E4", 2));
 
-    final var project2 =
-        List.of(
-            new Project("E1", 2), new Project("E2", 2), new Project("E3", 2), new Project("E4", 2));
+        final var project2 =
+                List.of(
+                        new Project("E1", 2),
+                        new Project("E2", 2),
+                        new Project("E3", 2),
+                        new Project("E4", 2));
 
-    var corporateEmployees = new ArrayList<CorporateEmployee>();
-    corporateEmployees.add(new CorporateEmployee(1, "Priyanka", project1));
-    corporateEmployees.add(new CorporateEmployee(2, "Zahid", project2));
+        var corporateEmployees = new ArrayList<CorporateEmployee>();
+        corporateEmployees.add(new CorporateEmployee(1, "Priyanka", project1));
+        corporateEmployees.add(new CorporateEmployee(2, "Zahid", project2));
 
-    var expected = InterviewProblemSolutions.employeesWorkedForMaxProjects(corporateEmployees);
+        var expected = InterviewProblemSolutions.employeesWorkedForMaxProjects(corporateEmployees);
 
-    CorporateEmployee actual = null; // Assert that the actual output matches the expected output
+        CorporateEmployee actual = null; // Assert that the actual output matches the expected output
 
-    Assertions.assertEquals(expected, actual);
-  }
+        Assertions.assertEquals(expected, actual);
+    }
 }
