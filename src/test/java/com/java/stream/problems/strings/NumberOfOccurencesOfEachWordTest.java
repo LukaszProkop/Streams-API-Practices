68c7c2b81e506d949cf9db50a0ae79f66cf88489
Lukasz_Prokop <Lukasz_Prokop@epam.com>
2024-11-07 14:45:13 +0100

    add solutions to NumberOfOccurencesOfEachWordTest

--- src/test/java/com/java/stream/problems/strings/NumberOfOccurencesOfEachWordTest.java
+++ src/test/java/com/java/stream/problems/strings/NumberOfOccurencesOfEachWordTest.java
@@ -2,30 +2,31 @@ package com.java.stream.problems.strings;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class NumberOfOccurencesOfEachWordTest {
     @Test
-    @Disabled("Remove This Once you Complete The Exercise")
     public void numberOfOccurencesOfEachWord() {
         String input = "the quick brown fox jumps right over the little lazy dog little";
-        final Map<String, Long> expectedOutput = Map.ofEntries(Map.entry("over", 1L),
-                Map.entry("the", 2L),
+        final Map<String, Long> expectedOutput = Map.ofEntries(Map.entry("the", 2L),
                 Map.entry("quick", 1L),
                 Map.entry("lazy", 1L),
                 Map.entry("jumps", 1L),
                 Map.entry("right", 1L),
+                Map.entry("over", 1L),
                 Map.entry("brown", 1L),
                 Map.entry("dog", 1L),
                 Map.entry("fox", 1L),
                 Map.entry("little", 2L));
 
-        String actualOutput = "<PUT YOUR LOGIC HERE>";
+        String actualOutput = Arrays.stream(input.split(" "))
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).toString();
         // Assert that the actual output matches the expected output
         assertEquals(expectedOutput, actualOutput);
     }
