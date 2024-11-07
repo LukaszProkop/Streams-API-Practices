b552b1bf0deff1fc3aefb0a89bfb0b83b419bae5
Lukasz_Prokop <Lukasz_Prokop@epam.com>
2024-11-07 15:09:56 +0100

    add solutions to SecondHighestWordTest

--- src/test/java/com/java/stream/problems/strings/SecondHighestWordTest.java
+++ src/test/java/com/java/stream/problems/strings/SecondHighestWordTest.java
@@ -6,16 +6,24 @@ import com.java.stream.solutions.GeneralProblemsSolution;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class SecondHighestWordTest {
-  @Test
-  @Disabled()
-  public void find2ndHighestWord() {
-    final String input = "I am interested123455 to grow in my organization";
-    String expected = GeneralProblemsSolution.find2ndLargestWordInTheString(input);
+    @Test
+    public void find2ndHighestWord() {
+        final String input = "I am interested123455 to grow in my organization";
+        String expected = GeneralProblemsSolution.find2ndLargestWordInTheString(input);
 
-    final String actual = "";
+        final String actual = Arrays.stream(input.split(" "))
+                .sorted(Comparator.comparingInt(String::length))
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .limit(1)
+                .collect(Collectors.joining());
 
-    assertEquals(expected, actual);
-  }
+        assertEquals(expected, actual);
+    }
 
 }
