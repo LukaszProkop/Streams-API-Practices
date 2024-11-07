edfe43f9a2be296b9c5bec87700ba5cddf14f142
Lukasz_Prokop <Lukasz_Prokop@epam.com>
2024-11-07 14:50:44 +0100

    add solutions to RemoveDuplicateCharactersTest

--- src/test/java/com/java/stream/problems/strings/RemoveDuplicateCharactersTest.java
+++ src/test/java/com/java/stream/problems/strings/RemoveDuplicateCharactersTest.java
@@ -6,12 +6,16 @@ import com.java.stream.solutions.GeneralProblemsSolution;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class RemoveDuplicateCharactersTest {
   @Test
-  @Disabled()
   public void testRemoveDuplicates() {
     final String input = "dabfcadef";
-    final String actual = "";
+    final String actual = Arrays.stream(input.split(""))
+            .distinct()
+            .collect(Collectors.joining());
     final String expected = GeneralProblemsSolution.getUniqueCharacters(input);
 
     assertEquals(expected, actual);
