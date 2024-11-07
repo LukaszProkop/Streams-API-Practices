29d6baa6fdb0a08c001391a611ac7791bd3b9732
Lukasz_Prokop <Lukasz_Prokop@epam.com>
2024-11-07 12:16:02 +0100

    add solutions to IntegersOnlyTest.java

--- src/test/java/com/java/stream/problems/numbers/IntegersOnlyTest.java
+++ src/test/java/com/java/stream/problems/numbers/IntegersOnlyTest.java
@@ -2,6 +2,8 @@ package com.java.stream.problems.numbers;
 
 import com.java.stream.solutions.GeneralProblemsNumbersSolution;
 import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -13,10 +15,11 @@ import org.junit.jupiter.api.Test;
  */
 public class IntegersOnlyTest {
     @Test
-    @Disabled
     void testCheckAllIntegersWithoutAlphabets(){
         var input = List.of("1","12","12a","a12");
-        var actual = List.of();
+        var actual = input.stream()
+                .filter(v -> v.matches("\\d+"))
+                .toList();
         var expected = GeneralProblemsNumbersSolution.getNumberOnly(input);
 
         Assertions.assertEquals(expected, actual);
